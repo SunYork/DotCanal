4f83d008374b9f3512e38c1137ee3bb69c65f485
姚正发 <976691141@qq.com>
2017-08-30 18:25:48 +0800

    改参考Mysql源码

--- src/DotCanal.Driver/Utils/MySQLPasswordEncrypter.cs
+++ src/DotCanal.Driver/Utils/MySQLPasswordEncrypter.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace DotCanal.Driver.Utils
 {
     public static class MySQLPasswordEncrypter
     {
+        public static byte[] Scramble411(byte[] pass, byte[] seed)
+        {
+            var sha1 = SHA1.Create();
+            var pass1 = sha1.ComputeHash(pass);
+            var pass2 = sha1.ComputeHash(pass1);
 
+            sha1.
+        }
     }
 }
